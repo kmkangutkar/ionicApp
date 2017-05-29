0e1b1ccb801dbd08f49cd02cd20bbcfbb1d3ef2e
kmkangutkar <kmkangutkar@users.noreply.github.com>
2017-05-29 16:21:22 +0530

    string comparison code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+    string_a = '';
+    string_b = '';
+    
+    get comparison_result(){
+        const a = this.string_a.toLowerCase();
+        const b = this.string_b.toLowerCase();
+        let min_length = 0;
+        if(a.length < b.length){
+            min_length = a.length;
+        }else{
+            min_length = b.length;
+        }
+        let same_chars = 0;
+        for (let i = 0; i < min_length; i++){
+            if(a[i] == b[i]){
+                same_chars++;
+            }
+        }
+        return same_chars;
+    }
+    
 
 }
