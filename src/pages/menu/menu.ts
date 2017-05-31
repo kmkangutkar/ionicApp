6cf9ecf6753eee820f69cfd50775d7b96a9e4db0
kmkangutkar <kmkangutkar@users.noreply.github.com>
2017-05-31 16:04:06 +0530

    Confirm order and add to cart

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the MenuPage page.
@@ -14,17 +14,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
 
+  cart: any = [];
   menuItems: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   	this.menuItems = [
-  		{name: "Pizza", details: "Cheese", price: "100"},
-  		{name: "Pasta", details: "Red Sauce", price: "105"},
-  		{name: "Ice Cream", details: "Vanilla", price: "50"}
+  		{name: "Pizza", details: "Cheese", price: "100", image: "../../assets/pictures/pizza-slice.png"},
+  		{name: "Pasta", details: "Red Sauce", price: "105", image: "../../assets/pictures/pasta.jpeg"},
+  		{name: "Ice Cream", details: "Vanilla", price: "50", image: "../../assets/pictures/icecream.png"}
   	]
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
+  }
+
+  showConfirm(item){
+  	let confirm = this.alertCtrl.create({
+  		title: "Place Order?",
+  		message: "Do you want to order " + item.name + " for Rs. " + item.price + "?",
+  		buttons: [
+  			{
+  				text: "Confirm",
+  				handler: () =>{
+  					console.log("Order Confirmed!");
+  					this.cart.push(item);
+  				}  	
+  			},
+  			{
+  				text: "Cancel",
+  				handler: () => {
+  					console.log("Order Cancelled!");
+  				}
+  			}
+  		]
+
+  	});
+  	confirm.present();	
+  }
+
+  whenClicked(item){
+  	console.log(item.price);
+  	this.showConfirm(item);
+	console.log(this.cart);
   }
 
 }
