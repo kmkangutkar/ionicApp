e768f9e4cdd8843a44592110fd5408cf4395ca6f
kmkangutkar <kmkangutkar@users.noreply.github.com>
2017-06-20 14:54:29 +0530

    load orders based on current user

--- src/pages/orders/orders.ts
+++ src/pages/orders/orders.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'; 
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
-
-/**
- * Generated class for the OrdersPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-orders',
   templateUrl: 'orders.html',
 })
 export class OrdersPage {
-
+  displayName: any;
   placedOrders: FirebaseListObservable<any[]>;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private angFireDB: AngularFireDatabase) {
-  	this.placedOrders = this.angFireDB.list('/Orders');
-  	
+  constructor(public navCtrl: NavController, public navParams: NavParams, private angFireDB: AngularFireDatabase,
+  				public authService: AuthServiceProvider) {
+  	this.displayName = this.authService.displayName();
+  	console.log(this.displayName);
+  	this.placedOrders = this.angFireDB.list('/Orders', {
+  		'query': {
+  			'orderByChild': 'userName',
+  			'equalTo': this.displayName
+  		}
+  	});
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrdersPage');
+    console.log('ionViewDidLoad OrdersPage', this.displayName);
   }
-
   
 }
 
